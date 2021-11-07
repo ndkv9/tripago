2827e6e9570f71a7564576e942115d1b445fb7f2
ndkv9 <vu.nguyen101@gmail.com>
2021-11-07 18:38:49 +0200

    add Trip-List component

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,9 +1,22 @@
-const TripList = () => {
+const TripList = ({ trips }) => {
   return (
-    <div>
+    <div className='trip-list'>
       <h2>Trip List</h2>
+      <ul>
+        {trips.map(trip => (
+          <Trip key={trip.id} trip={trip} />
+        ))}
+      </ul>
     </div>
   )
 }
 
+const Trip = ({ trip }) => {
+  return (
+    <li>
+      <h3>{trip.title}</h3>
+      <p>{trip.price}</p>
+    </li>
+  )
+}
 export default TripList
