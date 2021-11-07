151a1653d477b58d92094c3c50ce7345e6f8251d
ndkv9 <vu.nguyen101@gmail.com>
2021-11-07 18:25:45 +0200

    fetch data within useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css'
 import TripList from './components/TripList'
 import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [trips, setTrips] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/trips').then(({ data }) => setTrips(data))
+  }, [])
+
   return (
     <div className='App'>
-      <TripList />
+      <TripList trips={trips} />
     </div>
   )
 }
